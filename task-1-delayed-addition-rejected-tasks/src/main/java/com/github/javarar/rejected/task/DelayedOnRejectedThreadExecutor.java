977bb0b1768fa8c91b39354c110c452f65e3da64
Aidar <aidar_v2000@mail.ru>
2024-07-29 17:01:27 +0300

    Done task 1

--- task-1-delayed-addition-rejected-tasks/src/main/java/com/github/javarar/rejected/task/DelayedOnRejectedThreadExecutor.java
+++ task-1-delayed-addition-rejected-tasks/src/main/java/com/github/javarar/rejected/task/DelayedOnRejectedThreadExecutor.java
@@ -1,4 +1,49 @@
 package com.github.javarar.rejected.task;
 
-public class DelayedOnRejectedThreadExecutor {
+import java.util.concurrent.*;
+
+public class DelayedOnRejectedThreadExecutor extends ThreadPoolExecutor {
+
+    private final ScheduledExecutorService scheduledExecutor;
+    private final long rejectedRetrayDelayInTimeUnits;
+    private final TimeUnit rejectedRetrayDelayTimeUnit;
+
+    public DelayedOnRejectedThreadExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, long rejectedRetrayDelayInTimeUnits, TimeUnit rejectedRetrayDelayTimeUnit) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+        this.rejectedRetrayDelayInTimeUnits = rejectedRetrayDelayInTimeUnits;
+        this.rejectedRetrayDelayTimeUnit = rejectedRetrayDelayTimeUnit;
+        this.scheduledExecutor = Executors.newSingleThreadScheduledExecutor();
+        this.setRejectedExecutionHandler(this::rejectedExecution);
+    }
+
+    public DelayedOnRejectedThreadExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory, long rejectedRetrayDelayInTimeUnits, TimeUnit rejectedRetrayDelayTimeUnit) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory);
+        this.rejectedRetrayDelayInTimeUnits = rejectedRetrayDelayInTimeUnits;
+        this.rejectedRetrayDelayTimeUnit = rejectedRetrayDelayTimeUnit;
+        this.scheduledExecutor = Executors.newSingleThreadScheduledExecutor();
+        this.setRejectedExecutionHandler(this::rejectedExecution);
+    }
+
+    public DelayedOnRejectedThreadExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, RejectedExecutionHandler handler, long rejectedRetrayDelayInTimeUnits, TimeUnit rejectedRetrayDelayTimeUnit) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, handler);
+        this.rejectedRetrayDelayInTimeUnits = rejectedRetrayDelayInTimeUnits;
+        this.rejectedRetrayDelayTimeUnit = rejectedRetrayDelayTimeUnit;
+        this.scheduledExecutor = Executors.newSingleThreadScheduledExecutor();
+        this.setRejectedExecutionHandler(this::rejectedExecution);
+    }
+
+    public DelayedOnRejectedThreadExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory, RejectedExecutionHandler handler, long rejectedRetrayDelayInTimeUnits, TimeUnit rejectedRetrayDelayTimeUnit) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory, handler);
+        this.rejectedRetrayDelayInTimeUnits = rejectedRetrayDelayInTimeUnits;
+        this.rejectedRetrayDelayTimeUnit = rejectedRetrayDelayTimeUnit;
+        this.scheduledExecutor = Executors.newSingleThreadScheduledExecutor();
+        this.setRejectedExecutionHandler(this::rejectedExecution);
+    }
+
+    private void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+        scheduledExecutor.schedule(() -> {
+            System.out.printf("Retry to add rejected task: %s%n", r);
+            executor.execute(r);
+        }, rejectedRetrayDelayInTimeUnits, rejectedRetrayDelayTimeUnit);
+    }
 }
--- task-1-delayed-addition-rejected-tasks/src/test/java/com/github/javarar/rejected/task/DelayedOnRejectedThreadExecutorTest.java
+++ task-1-delayed-addition-rejected-tasks/src/test/java/com/github/javarar/rejected/task/DelayedOnRejectedThreadExecutorTest.java
@@ -1,6 +1,44 @@
 package com.github.javarar.rejected.task;
 
-public class DelayedOnRejectedThreadExecutorTest {
+import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class DelayedOnRejectedThreadExecutorTest {
+    private final int TASK_COUNT = 30;
+    @Test
+    void testName() throws InterruptedException {
+        DelayedOnRejectedThreadExecutor executor = new DelayedOnRejectedThreadExecutor(
+                3,
+                4,
+                100,
+                TimeUnit.MILLISECONDS,
+                new ArrayBlockingQueue<>(5),
+                200,
+                TimeUnit.MILLISECONDS
+        );
+
+        for (int i = 0; i < TASK_COUNT; i++) {
+            int finalI = i;
+            executor.execute(() -> {
+                try {
+                    Thread.sleep(5000);
+                    System.out.printf("Finish task #: %s%n", finalI);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+            });
+        }
+
+        while(executor.getActiveCount()!=0){
+
+        }
+        assertEquals(executor.getCompletedTaskCount(), TASK_COUNT);
+    }
 
 }
