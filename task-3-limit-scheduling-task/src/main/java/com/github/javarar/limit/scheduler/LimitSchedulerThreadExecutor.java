4d408b20870457c9a94a53311e577cd44216ceb5
Aidar <aidar_v2000@mail.ru>
2024-07-30 12:23:17 +0300

    Done task 3

--- task-3-limit-scheduling-task/src/main/java/com/github/javarar/limit/scheduler/LimitSchedulerThreadExecutor.java
+++ task-3-limit-scheduling-task/src/main/java/com/github/javarar/limit/scheduler/LimitSchedulerThreadExecutor.java
@@ -1,4 +1,68 @@
 package com.github.javarar.limit.scheduler;
 
-public class LimitSchedulerThreadExecutor {
-}
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class LimitSchedulerThreadExecutor extends ScheduledThreadPoolExecutor {
+
+    public LimitSchedulerThreadExecutor(int corePoolSize) {
+        super(corePoolSize);
+    }
+
+    public LimitSchedulerThreadExecutor(int corePoolSize, ThreadFactory threadFactory) {
+        super(corePoolSize, threadFactory);
+    }
+
+    public LimitSchedulerThreadExecutor(int corePoolSize, RejectedExecutionHandler handler) {
+        super(corePoolSize, handler);
+    }
+
+    public LimitSchedulerThreadExecutor(int corePoolSize, ThreadFactory threadFactory, RejectedExecutionHandler handler) {
+        super(corePoolSize, threadFactory, handler);
+    }
+
+    public ScheduledFuture<?> scheduleAtFixedRate(Runnable command,
+                                                  long initialDelay,
+                                                  long period,
+                                                  TimeUnit unit, Long maxExecutionCount) {
+        if (maxExecutionCount == null) {
+            super.scheduleAtFixedRate(command, initialDelay, period, unit);
+        }
+        if (maxExecutionCount < 1) {
+            throw new IllegalArgumentException();
+        }
+        AtomicLong availableAttemts = new AtomicLong(maxExecutionCount);
+        return super.scheduleAtFixedRate(() -> {
+            if (availableAttemts.get() > 0) {
+                command.run();
+                availableAttemts.decrementAndGet();
+            } else {
+                this.remove(command);
+            }
+
+        }, initialDelay, period, unit);
+    }
+
+
+    public ScheduledFuture<?> scheduleWithFixedDelay(Runnable command,
+                                                     long initialDelay,
+                                                     long delay,
+                                                     TimeUnit unit, Long maxExecutionCount) {
+        if (maxExecutionCount == null) {
+            super.scheduleWithFixedDelay(command, initialDelay, delay, unit);
+        }
+        if (maxExecutionCount < 1) {
+            throw new IllegalArgumentException();
+        }
+        AtomicLong availableAttemts = new AtomicLong(maxExecutionCount);
+        return super.scheduleWithFixedDelay(() -> {
+            if (availableAttemts.get() > 0) {
+                command.run();
+                availableAttemts.decrementAndGet();
+            } else {
+                this.remove(command);
+            }
+
+        }, initialDelay, delay, unit);
+    }
+    }
--- task-3-limit-scheduling-task/src/test/java/com/github/javarar/limit/scheduler/LimitSchedulerThreadExecutorTest.java
+++ task-3-limit-scheduling-task/src/test/java/com/github/javarar/limit/scheduler/LimitSchedulerThreadExecutorTest.java
@@ -1,4 +1,29 @@
 package com.github.javarar.limit.scheduler;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class LimitSchedulerThreadExecutorTest {
+    @Test
+    void testExecutionsCount() throws InterruptedException {
+        LimitSchedulerThreadExecutor executor = new LimitSchedulerThreadExecutor(2);
+
+        AtomicInteger executionCountAtFixedRate = new AtomicInteger();
+        executor.scheduleAtFixedRate(executionCountAtFixedRate::incrementAndGet, 0, 100, TimeUnit.MILLISECONDS, 10L);
+
+        AtomicInteger executionCountWithFixedDelay = new AtomicInteger();
+        executor.scheduleWithFixedDelay(executionCountWithFixedDelay::incrementAndGet, 10, 200, TimeUnit.MILLISECONDS, 15L);
+
+        while(!executor.getQueue().isEmpty()){
+
+        }
+        assertAll(
+                () -> assertEquals(10, executionCountAtFixedRate.get()),
+                () -> assertEquals(15, executionCountWithFixedDelay.get()));
+    }
 }
